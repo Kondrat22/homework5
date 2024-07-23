8493993b20c69668c740d28d26486d9edc2d41c2
Kondrat22 <kondrat_va@bk.ru>
2024-07-23 14:30:29 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = "домашка", 5, 1+2, 1.1
+#immutable_var[0] = задание
+print (immutable_var)
+mutable_list = ["домашка", 5, 1+2, 1.1]
+mutable_list[0] = "задание"
+mutable_list[2] = ''
+print (mutable_list)
